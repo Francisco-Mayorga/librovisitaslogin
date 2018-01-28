2f852a6d1f1346ea0174d681d09f0f2b60b0656d
Francisco Mayorga <fffmayorga@gmail.com>
2018-01-28 14:41:17 +0100

    Initial commit

---
+++ models.py
@@ -0,0 +1,7 @@
+from google.appengine.ext import ndb
+
+class Message(ndb.Model):
+    nombre = ndb.StringProperty()
+    email = ndb.StringProperty()
+    texto = ndb.TextProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
